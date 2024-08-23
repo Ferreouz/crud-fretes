81875e538482de2655eba7c79370f292dfa44f93
Ferreouz <g.f.guimaraes@hotmail.com>
2024-08-23 11:38:22 -0300

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# TODO
+- Frontend
+- Backend
+- Tests
+- Install on Docker
+- Create live demo
---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='app'>
+      <p>Hello</p>
+     </div>
+    </>
+  )
+}
+
+export default App
