f015d164d14dab3d529b70c3e16d0268425e3c4c
Ferreouz <g.f.guimaraes@hotmail.com>
2024-08-26 11:20:31 -0300

    navbar refactor

---
+++ frontend/src/components/ MNavbarCompany/index.tsx
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { useNavigate } from "react-router-dom";
+
+function MNavbarCompany() {
+    const navigate = useNavigate();
+    return (
+        <>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link onClick={() => navigate("/")}>Fretes</Nav.Link>
+                            <Nav.Link onClick={() => navigate("/veiculos")}>Veiculos</Nav.Link>
+                            <Nav.Link onClick={() => navigate("/usuarios")}>Usuarios</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default MNavbarCompany
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -5,6 +5,7 @@ import { Freight } from "../../types";
 import { getFreights } from "../../hooks/Freight";
 import ModalFreight from "./ModalFreight";
 import { PropsModalFreight } from "./types";
+import MNavbarCompany from "../../components/ MNavbarCompany";
 
 function Home() {
   const [freights, setFreights] = useState<Freight[]>([]);
@@ -21,53 +22,58 @@ function Home() {
   }, []);
 
   return (
-    <div className="container">
-      <ModalFreight opened={showModal} closeModal={() => setModalState(false)} freight={freightEdit}
-        addFreight={(newFreight: Freight) => setFreights(freights.push(newFreight) ? freights : freights)}
-        editFreight={(newFreight: Freight) => {
-          for (let index = 0; index < freights.length; index++) {
-            const element = freights[index];
-            if(element.id && element.id == newFreight.id ){
-              Object.assign(freights[index],newFreight);
-            }            
-          }
-        }}
-        operation={operation as PropsModalFreight["operation"]}
-      />
-      <div className="row">
-        <h3 className="col-6">Todos os Fretes</h3>
-        <div className=" col-md-3 offset-md-3">
-          <Button variant="primary" onClick={() => {
-            setOperation("create");
-            setFreightEdit({});
-            setModalState(true);
-          }}>
-            Anunciar Frete
-          </Button>
-        </div>
-      </div>
-      <div className="container text-center">
+    <>
+      <MNavbarCompany />
+      <div className="container">
+        <ModalFreight opened={showModal} closeModal={() => setModalState(false)} freight={freightEdit}
+          addFreight={(newFreight: Freight) => setFreights(freights.push(newFreight) ? freights : freights)}
+          editFreight={(newFreight: Freight) => {
+            for (let index = 0; index < freights.length; index++) {
+              const element = freights[index];
+              if (element.id && element.id == newFreight.id) {
+                Object.assign(freights[index], newFreight);
+              }
+            }
+          }}
+          operation={operation as PropsModalFreight["operation"]}
+        />
         <div className="row">
-          {freights?.map((item: Freight) => (
-            <Card
-              key={item.id}
-              title={item.productName || ""}
-              subtitle={"R$" + item.price}
-              text={[`Veículo: ${item.vehicle?.plate}`, `Status: ${item.opened ? "Aberto" : "Aguardando Motorista"}`]}
-              onEdit={() => {
-                setOperation("update");
-                setFreightEdit(item);
-                setModalState(true);
-              }}
-              onDelete={() => { }}
-              canEdit={item.opened == true}
-              canDelete={item.opened == true}
-              lastUpdate={item.updated_at}
-            />
-          ))}
+          <h3 className="col-6">Todos os Fretes</h3>
+          <div className=" col-md-3 offset-md-3">
+            <Button variant="primary" onClick={() => {
+              setOperation("create");
+              setFreightEdit({});
+              setModalState(true);
+            }}>
+              Anunciar Frete
+            </Button>
+          </div>
+        </div>
+        <div className="container text-center">
+          <div className="row">
+            {freights?.map((item: Freight) => (
+              <Card
+                key={item.id}
+                title={item.productName || ""}
+                subtitle={"R$" + item.price}
+                text={[`Veículo: ${item.vehicle?.plate}`, `Status: ${item.opened ? "Aberto" : "Aguardando Motorista"}`]}
+                onEdit={() => {
+                  setOperation("update");
+                  setFreightEdit(item);
+                  setModalState(true);
+                }}
+                onDelete={() => { }}
+                canEdit={item.opened == true}
+                canDelete={item.opened == true}
+                lastUpdate={item.updated_at}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+
+    </>
+
   );
 }
 
