0d70ffbd32dd9080840a9a8b7f9de7020dac1555
Ferreouz <g.f.guimaraes@hotmail.com>
2024-08-28 11:56:10 -0300

    fix: deactivated message even on wrong password

--- backend/src/api/routes/login.ts
+++ backend/src/api/routes/login.ts
@@ -18,21 +18,23 @@ export default function route(app) {
     
             const user = await db.users.get(email);
             const correctPass = bcrypt.compareSync(password, user?.password || whateverPass);
-    
-            if (user && user.active == false) {
+
+            if (!user || !user.email || !correctPass) {
+                return res.status(401).json({
+                    error: "Credenciais Inválidas"
+                });
+            }
+
+            if (user.active == false) {
                 return res.status(401).json({
                     error: "Sua conta foi desativada, entre em contato com o suporte para saber o motivo"
                 });
             }
-            if (user && user.email && correctPass) {
-                delete user.password;
-                const access_token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
-                return res.json({ access_token });
-            }
-    
-            return res.status(401).json({
-                error: "Credenciais Inválidas"
-            });
+
+            delete user.password;
+            const access_token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+            return res.json({ access_token });
+            
         } catch (error) {
             console.log(error)
             return res.status(401).json({
