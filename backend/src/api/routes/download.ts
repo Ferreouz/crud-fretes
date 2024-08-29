8b6838491a4242d5e199d63eebee3f240549519f
Ferreouz <g.f.guimaraes@hotmail.com>
2024-08-28 23:21:27 -0300

    add generate report in API for testing purposes

--- backend/src/api/routes/download.ts
+++ backend/src/api/routes/download.ts
@@ -2,6 +2,7 @@ import middleware from "../middleware";
 import type { Request, Response } from 'express';
 import isAdmin from "../isadmin";
 import { existsSync } from "fs";
+import { generateReport } from "../../utils/generateReport";
 export default function route(app) {
     app.get("/report/yesterday", middleware, async (req: Request, res: Response) => {
         if (!isAdmin(req.user)) {
@@ -16,6 +17,19 @@ export default function route(app) {
             res.setHeader('Content-disposition', 'attachment; filename=' + file);
             res.setHeader('Content-type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
             res.download(fullPath);
+        } catch (error) {
+            return res.sendStatus(400);
+        }
+    });
+
+    //for testing purposes
+    app.post("/report/yesterday/generate", middleware, async (req: Request, res: Response) => {
+        if (!isAdmin(req.user)) {
+            return res.sendStatus(403);
+        }
+        try {
+            await generateReport();
+            return res.sendStatus(200);
         } catch (error) {
             return res.sendStatus(400);
         }
